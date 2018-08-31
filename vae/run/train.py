d23b200dffdcf7570e7f589596d5947dbbde69b9
Paweł Kubik <p.kubik.pl@gmail.com>
2018-08-31 11:38:21 +0200

    Creating symlink to the latest model

--- vae/run/train.py
+++ vae/run/train.py
@@ -1,10 +1,18 @@
 import sys
 
 from contextlib import ExitStack
+from pathlib import Path
 
 import vae.configurator as cfg
 from vae.data import load_mnist, default_models_dir, open_config
 from vae.model import Model
+
+
+def create_latest_model_symlink(model_path: Path):
+    models_dir = default_models_dir()
+    latest_path = model_path / 'symlink'
+    latest_path.symlink_to(model_path, target_is_directory=True)
+    latest_path.replace(models_dir / 'latest')
 
 
 def train(model_name: str, config_name: str = None):
@@ -14,7 +22,9 @@ def train(model_name: str, config_name: str = None):
 
         data = load_mnist()
 
-        model = Model(default_models_dir() / model_name)
+        model_path = default_models_dir() / model_name
+        model = Model(model_path)
+        create_latest_model_symlink(model_path)
         model.train(data.train)
 
 
