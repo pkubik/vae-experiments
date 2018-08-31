c5ac80056eea40f10258ddaf5544b40c64488f64
Paweł Kubik <p.kubik.pl@gmail.com>
2018-08-31 23:42:11 +0200

    Use `tf.estimator` API

--- vae/model.py
+++ vae/model.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 
-from contextlib import suppress
 from pathlib import Path
 from typing import Union
 
@@ -13,30 +12,48 @@ from vae.net.vi import VAE
 from vae.plot import PlotSaverHook
 
 
-class DataIterator:
-    def __init__(self, batch_size: int, data: Dataset):
-        images = norm_images(data.images)
-        labels = data.labels
-        self.data = Dataset(images, labels)
+class ProgressPrinterHook(tf.train.SessionRunHook):
+    Fetches = namedtuple('Fetches', 'step, rec_loss, reg_loss')
 
-        self.images_placeholder = tf.placeholder(images.dtype, images.shape)
-        self.labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
+    def __init__(self, steps_before_eval, rec_loss, reg_loss):
+        self.step = tf.train.get_global_step()
+        self.steps_before_eval = steps_before_eval
+        self.rec_loss = rec_loss
+        self.reg_loss = reg_loss
+        self.progbar = tf.keras.utils.Progbar(
+            self.steps_before_eval,
+            stateful_metrics=['rec loss', 'reg loss'])
 
-        dataset = tf.data.Dataset.from_tensor_slices(
-            Dataset(self.images_placeholder, self.labels_placeholder))
-        self.iterator = (dataset
-                         .shuffle(cfg.get('shuffle_buffer_size', 2048))
-                         .batch(batch_size)
-                         .repeat(1)
-                         .map(lambda x: Dataset(tf.expand_dims(x.images, -1), x.labels))
-                         .make_initializable_iterator())
-        self.next = self.iterator.get_next()
+    def before_run(self, run_context):
+        return tf.train.SessionRunArgs(
+            self.Fetches(self.step, self.rec_loss, self.reg_loss))
 
-    def initialize(self, sess: tf.Session):
-        sess.run(self.iterator.initializer, feed_dict={
-            self.images_placeholder: self.data.images,
-            self.labels_placeholder: self.data.labels
-        })
+    def after_run(self, run_context, run_values):
+        fetches = run_values.results  # type: ProgressPrinterHook.Fetches
+        i = fetches.step % self.steps_before_eval
+        if i == 0:
+            self.progbar = tf.keras.utils.Progbar(
+                self.steps_before_eval,
+                stateful_metrics=['rec loss', 'reg loss'])
+            print()
+        else:
+            self.progbar.update(i, [
+                ('rec loss', fetches.rec_loss),
+                ('reg loss', fetches.reg_loss)])
+
+
+def create_input_fn(batch_size: int, data: Dataset):
+    images = norm_images(data.images)
+    labels = data.labels
+
+    input_fn = tf.estimator.inputs.numpy_input_fn(
+        images, labels,
+        batch_size, 1,
+        shuffle=True,
+        queue_capacity=cfg.get('shuffle_buffer_size', 2048)
+    )
+
+    return input_fn
 
 
 class Model:
@@ -50,48 +67,28 @@ class Model:
         self.model_dir = Path(model_dir)
         self.model_dir.mkdir(parents=True, exist_ok=True)
 
+    def build(self, features, labels, mode):
+        features = tf.expand_dims(features, -1)
+        vae = VAE(features, labels)
+        vae_train = vae.create_train_spec()
+
+        plot_saver_hook = PlotSaverHook(self.model_dir / 'plots', vae, self.steps_before_eval)
+        progress_printer_hook = ProgressPrinterHook(
+            self.steps_before_eval,
+            vae_train.vlb.reconstruction_loss,
+            vae_train.vlb.regularization_loss)
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=vae_train.vlb.total_loss,
+            train_op=vae_train.train_op,
+            training_chief_hooks=[plot_saver_hook, progress_printer_hook])
+
     def train(self, data: Dataset):
-        graph = tf.Graph()
-        with graph.as_default():
-            global_step = tf.train.get_or_create_global_step()
+        estimator = tf.estimator.Estimator(self.build, self.model_dir)
+        save_config(cfg.current(), self.model_dir / 'config.yml')
 
-            data_iterator = DataIterator(self.batch_size, data)
-
-            vae = VAE(data_iterator.next.images, data_iterator.next.labels)
-            vae_train = vae.create_train_spec()
-
-            plot_saver_hook = PlotSaverHook(self.model_dir / 'plots', vae, self.steps_before_eval)
-
-            save_config(cfg.current(), self.model_dir / 'config.yml')
-
-            with tf.train.MonitoredTrainingSession(
-                    checkpoint_dir=str(self.model_dir),
-                    config=self.config_proto,
-                    hooks=[plot_saver_hook]) as sess:
-                for epoch in range(self.num_epochs):
-                    print("Starting epoch {}".format(epoch))
-
-                    progbar = tf.keras.utils.Progbar(self.steps_before_eval)
-
-                    with suppress(tf.errors.OutOfRangeError):
-                        data_iterator.initialize(sess._tf_sess())
-
-                        while not sess.should_stop():
-                            Fetches = namedtuple('Fetches',
-                                                 'global_step,'
-                                                 'train_op, rec_loss, reg_loss')
-                            fetches = Fetches(global_step,
-                                              vae_train.train_op,
-                                              vae_train.vlb.reconstruction_loss,
-                                              vae_train.vlb.regularization_loss)
-                            outputs = sess.run(fetches)
-
-                            i = outputs.global_step % self.steps_before_eval
-                            if i == 0:
-                                progbar = tf.keras.utils.Progbar(self.steps_before_eval)
-
-                            progbar.update(i, [
-                                ('rec loss', outputs.rec_loss),
-                                ('reg loss', outputs.reg_loss)])
-
-                    print()
+        for epoch in range(self.num_epochs):
+            print("Starting epoch {}".format(epoch))
+            estimator.train(create_input_fn(self.batch_size, data))
+            print()
